dae3edf44ced43462e40c2adb86f2645de6c0632
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-03-11 11:26:18 +0630

    auth facade and auth failure

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import 'auth_failure.dart';
+import 'value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
